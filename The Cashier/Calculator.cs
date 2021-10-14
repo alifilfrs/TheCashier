557a0a06840c8459f7683b3578412d6d52a16419
Alifil <Alifil@LAPTOP-PH315-53>
2021-10-14 08:42:06 +0700

    Add project files.

---
+++ The Cashier/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCashier
+{
+    class Calculator
+    {
+        private List<Item> listItem;
+        private double total = 0;
+
+        public Calculator()
+        {
+            this.listItem = new List<Item>();
+        }
+        public void addItem(Item item)
+        {
+            this.listItem.Add(item);
+            this.total += item.getSubTotal();
+        }
+        public double getTotal()
+        {
+            return total;
+        }
+
+        public List<Item> getListItem()
+        {
+            return listItem;
+        }
+    }
+}
---
+++ The Cashier/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCashier
+{
+    class Item
+    {
+        private int id;
+        public string title { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+
+        public Item(int id, string title, int quantity, string type, double price)
+        {
+            this.id = id;
+            this.title = title;
+            this.quantity = quantity;
+            this.type = type;
+            this.price = price;
+            this.subtotal = 0;
+        }
+        public string getTitle()
+        {
+            return title;
+        }
+        public int getQuantity()
+        {
+            return quantity;
+        }
+        public string getType()
+        {
+            return type;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+        public double getSubTotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }
+    }
+}
